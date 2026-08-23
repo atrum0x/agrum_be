2fa27e20ce1055a0a675be02ebb8d656cafc3573
Senuja0x <senujajayasekara@gmail.com>
2026-08-24 00:46:14 +0530

    first commit

---
+++ src/main/java/com/atrum/agrum/AgrumApplication.java
@@ -0,0 +1,13 @@
+package com.atrum.agrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrumApplication.class, args);
+	}
+
+}
